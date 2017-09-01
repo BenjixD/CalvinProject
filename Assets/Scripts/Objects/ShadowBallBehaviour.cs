bb4fd0d1cd019ac141507c30b6beffdcb4c06715
Leo <lileo-jh@gmail.com>
2017-09-01 01:10:19 -0400

    Changed check for Player to use layer instead of tag

--- Assets/Scripts/Objects/ShadowBallBehaviour.cs
+++ Assets/Scripts/Objects/ShadowBallBehaviour.cs
@@ -43,7 +43,7 @@ public class ShadowBallBehaviour : MonoBehaviour, IBullet
     #region Trigger Handlers
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.layer.Equals(LayerMask.NameToLayer("Player")))
         {
             OnHit(collision.gameObject);
         }
