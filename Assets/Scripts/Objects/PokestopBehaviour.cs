6b64a62e6d19701e9f4f6472a835497c75249fea
Leo <leojh.li@mail.utoronto.ca>
2017-09-02 17:10:22 -0400

    Created Pokestop fade to purple effect

--- Assets/Scripts/Objects/PokestopBehaviour.cs
+++ Assets/Scripts/Objects/PokestopBehaviour.cs
@@ -5,34 +5,63 @@ using UnityEngine;
 public class PokestopBehaviour : MonoBehaviour {
 
     #region Public Members
-    public GameObject[] ChildrenToAnimate;
-    public float DestroyDelay;
+    public GameObject BlueStop;
+    public GameObject PurpleStop;
+    public float FadeDuration;
     #endregion
 
     #region Private Members
     bool m_used;
+    float m_minAlpha = 0f;
+    float m_maxAlpha = 1f;
+    float m_startTime;
+    float m_smoothStepDuration = 2f;
     #endregion
 
     // Use this for initialization
     void Start () {
         m_used = false;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
     #region Helper Functions
     private void SpinningAnim()
     {
-		foreach (GameObject child in ChildrenToAnimate) {
-			child.GetComponent<Animator>().SetTrigger("TriggerSpin");
-		}
+        foreach (Transform child in BlueStop.transform)
+        {
+            if(child.name == "PokestopEye")
+            {
+                child.GetComponent<Animator>().SetTrigger("TriggerSpin");
+            }
+        }
+        foreach (Transform child in PurpleStop.transform)
+        {
+            if (child.name == "PokestopEye")
+            {
+                child.GetComponent<Animator>().SetTrigger("TriggerSpin");
+
+            }
+        }
     }
 
-	private void FadeToPurple(){
-		
+	IEnumerator FadeToPurple(){
+        for(;;)
+        {
+            float t = (Time.time - m_startTime) / FadeDuration;
+            foreach (SpriteRenderer renderer in BlueStop.GetComponentsInChildren<SpriteRenderer>())
+            {
+                renderer.color = new Color(1f, 1f, 1f, 1 - Mathf.SmoothStep(m_minAlpha, m_maxAlpha, t));
+            }
+            if (t >= m_smoothStepDuration)
+            {
+                yield break;
+            }
+            yield return null;
+        }
 	}
     #endregion
 
@@ -45,7 +74,8 @@ public class PokestopBehaviour : MonoBehaviour {
             {
                 m_used = true;
                 SpinningAnim();
-				FadeToPurple();
+                m_startTime = Time.time;
+                StartCoroutine("FadeToPurple");
             }
         }
     }
