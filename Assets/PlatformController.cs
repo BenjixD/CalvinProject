dd1b0dcd76b41e2a0a88cc8c0b0f002df6546083
Leo <leojh.li@mail.utoronto.ca>
2017-09-02 22:34:09 -0400

    Created platform layout cycling

--- Assets/PlatformController.cs
+++ Assets/PlatformController.cs
@@ -8,20 +8,32 @@ public class PlatformController : MonoBehaviour {
     public GameObject[] PlatformLayouts;
     public float TranslucentPlatformAlpha;
     public float TranslucentWaitTime;
+    public float LayoutDuration;                    // How long a layout lasts. Must be greater than
+                                                    // TranscluentWaitTime.
+    public float LayoutlessDuration;                // Time between layout being destroyed and the next being
+                                                    // spawned. Must be greater than LayoutFadeDuration.
+
+    public float LayoutFadeDuration;
+    public float DeactivateCollidersDelay;          // How long the platforms' colliders persist as they fade.
+    #endregion
+
+    #region Private Members
+    private GameObject m_currentLayout;
+    private float m_minAlpha = 0f;
+    private float m_maxAlpha = 1f;
+    private float m_fadeStartTime;
+    private float m_smoothStepDuration = 1f;
     #endregion
 
     // Use this for initialization
-    void Start () {
+    void Start() {
+        StartCoroutine(CyclePlatformLayouts());
+    }
 
-        // TODO: if all platforms are gone, and after x seconds:
-        StartCoroutine(SpawnPlatformLayout(GetRandomLayoutIndex()));
+    // Update is called once per frame
+    void Update() {
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
     #region Helper Functions
     int GetRandomLayoutIndex()
@@ -29,18 +41,23 @@ public class PlatformController : MonoBehaviour {
         return Random.Range(0, PlatformLayouts.Length);
     }
 
-    void DeactivateLayout(GameObject layout)
+    void PrepareLayout(GameObject layout)
     {
         foreach (SpriteRenderer renderer in layout.GetComponentsInChildren<SpriteRenderer>())
         {
             renderer.color = new Color(1f, 1f, 1f, TranslucentPlatformAlpha);
         }
+        DeactivateLayoutColliders(layout);
+    }
+
+    void DeactivateLayoutColliders(GameObject layout)
+    {
         foreach (EdgeCollider2D collider in layout.GetComponentsInChildren<EdgeCollider2D>())
         {
             collider.enabled = false;
         }
     }
-    
+
     void ActivateLayout(GameObject layout)
     {
         foreach (SpriteRenderer renderer in layout.GetComponentsInChildren<SpriteRenderer>())
@@ -54,14 +71,53 @@ public class PlatformController : MonoBehaviour {
     }
     #endregion
 
-    #region Coroutine
+    #region Coroutines
+    IEnumerator CyclePlatformLayouts()
+    {
+        for (;;)
+        {
+            yield return new WaitForSeconds(LayoutlessDuration);
+            StartCoroutine(SpawnPlatformLayout(GetRandomLayoutIndex()));
+            yield return new WaitForSeconds(LayoutDuration);
+            m_fadeStartTime = Time.time;
+            StartCoroutine(FadeOutPlatformLayout());
+        }
+    }
+
     IEnumerator SpawnPlatformLayout(int layoutIndex)
     {
-        GameObject layout = Instantiate(PlatformLayouts[layoutIndex]);
-        DeactivateLayout(layout);
+        Debug.Log("Begin Spawn");
+
+        m_currentLayout = Instantiate(PlatformLayouts[layoutIndex]);
+        PrepareLayout(m_currentLayout);
         yield return new WaitForSeconds(TranslucentWaitTime);
-        ActivateLayout(layout);
+        ActivateLayout(m_currentLayout);
         yield return null;
     }
+
+    IEnumerator FadeOutPlatformLayout()
+    {
+        Debug.Log("Begin Fadeout");
+
+        for (;;)
+        {
+            float t = (Time.time - m_fadeStartTime) / LayoutFadeDuration;
+            foreach (SpriteRenderer renderer in m_currentLayout.GetComponentsInChildren<SpriteRenderer>(true))
+            {
+                renderer.color = new Color(1f, 1f, 1f, 1 - Mathf.SmoothStep(m_minAlpha, m_maxAlpha, t));
+            }
+            if (t >= DeactivateCollidersDelay)
+            {
+                DeactivateLayoutColliders(m_currentLayout);
+            }
+            if (t >= m_smoothStepDuration)
+            {
+                Destroy(m_currentLayout);
+                yield break;
+            }
+            yield return null;
+        }
+    }
     #endregion
+
 }
