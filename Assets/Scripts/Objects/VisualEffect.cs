c4a44b51fb7a15d30f2f8d3d6178db900c7fd4d5
Leo <leojh.li@mail.utoronto.ca>
2017-09-02 23:27:20 -0400

    Created meteor and explosion on collision

---
+++ Assets/Scripts/Objects/VisualEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualEffect : MonoBehaviour
+{
+
+    #region Public Members
+    public AnimationClip anim;
+    #endregion
+
+    #region Private Member
+    private Animator m_animator;
+    #endregion
+
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (m_animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.95f && !m_animator.IsInTransition(0))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
